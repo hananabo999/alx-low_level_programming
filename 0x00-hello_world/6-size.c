5e8861e28ae15d16e5407292d9ef7b5872a522ec
Your Name <you@example.com>
2023-08-10 16:34:21 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-        printf("size of a char: %d byte(s)", sizeof(a));
-        printf("size of an int: %d byte(s)", sizeof(b));
-        printf("size of a long int: %d byte(s)", sizeof(c));
-        printf("size of a long long int: %d byte(s)", sizeof(d));
-        printf("size of a float: %d byte(s)", sizeof(f));
+        printf("size of a char: %d byte(s)", sizeof(char));
+        printf("size of an int: %d byte(s)", sizeof(int));
+        printf("size of a long int: %d byte(s)", sizeof(long int));
+        printf("size of a long long int: %d byte(s)", sizeof(long long int));
+        printf("size of a float: %d byte(s)", sizeof(float));
         return (0);
 }
