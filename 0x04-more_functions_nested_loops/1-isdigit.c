c361bc11016b6fb60f91032d7f563f64afebc107
Your Name <you@example.com>
2023-08-17 16:21:13 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{   
+    int number = 1;
+    int otherwise = 0;
+    if (c >= '0' && c <= '9')
+		return number;
+
+    else
+		return otherwise;
+}
