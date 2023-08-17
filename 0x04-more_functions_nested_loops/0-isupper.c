a9e0c720e8bbcafd1a1a38d0c3d5e38fff9c15a4
Your Name <you@example.com>
2023-08-17 16:28:27 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,11 +8,13 @@
  * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
-{ 
+{
+
     if (c >= 'A' && c <= 'Z'){
 	    return 1;
     }
     else{
 	    return 0;
     }
+
 }
